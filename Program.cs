dc4d86b7f9c389c941894f477468bc55b445bccd
Halloul Tarek <46580949+T-Fluffy@users.noreply.github.com>
2024-02-11 04:01:32 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+using System.Drawing;
+
+namespace ImageToPdfConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: ImageToPdfConverter <inputFolder> <outputPdfFile>");
+                return;
+            }
+
+            string inputFolder = args[0];
+            string outputPdfFile = args[1];
+
+            ConvertToPdf(inputFolder, outputPdfFile);
+        }
+
+        static void ConvertToPdf(string inputFolder, string outputPdfFile)
+        {
+            string[] imageFiles = Directory.GetFiles(inputFolder, "*.png");
+
+            using (PdfDocument pdf = new PdfDocument())
+            {
+                foreach (string imageFile in imageFiles)
+                {
+                    using (Bitmap bitmap = new Bitmap(imageFile))
+                    {
+                        PdfPage page = pdf.AddPage();
+                        page.Width = bitmap.Width * 72 / bitmap.HorizontalResolution;
+                        page.Height = bitmap.Height * 72 / bitmap.VerticalResolution;
+                        XGraphics gfx = XGraphics.FromPdfPage(page);
+
+                        XImage xImage = XImage.FromFile(imageFile);
+                        gfx.DrawImage(xImage, 0, 0, page.Width, page.Height);
+                    }
+                }
+                pdf.Save(outputPdfFile);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Images_To_Pdf_Converter_V1
+
+This is a simple DOTNET7 console application for converting multiple images to a single PDF file, whiteout modifying their dimensions.
+
+## Packages used
+
+* The PdfSharp Nuget Package, version 6.0.0.
+* The System.Drawing.Common Nuget Package, version 8.0.1.
